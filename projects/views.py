86724d4767ae6b9ed568e98965bf3d92236f05e1
Maria <marycarol.00@gmail.com>
2024-09-03 20:47:04 -0300

    feat: viewset customizada para Profile

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,29 @@
-#  from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Profile, Project
 from projects.serializer import ProfileSerializer, ProjectSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            profile = get_object_or_404(Profile, pk=kwargs['pk'])
+
+            return render(
+                request,
+                'profile_detail.html',
+                {'profile': profile}
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
